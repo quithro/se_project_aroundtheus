0707dcefa8d96b3f7305cef44f516f3311b29423
quithro <143888319+quithro@users.noreply.github.com>
2023-10-02 21:38:51 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-const initialCards = [
+let initialCards = [ 
     {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+        name: "Yosemite Valley",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
     },
     {
         name: "Lake Louise",
