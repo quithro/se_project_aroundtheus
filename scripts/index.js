5718a2119519f764c398d12c2fd4ed68eedecd07
quithro <143888319+quithro@users.noreply.github.com>
2023-10-09 07:20:21 -0500

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -41,7 +41,7 @@ const cardTemplate = document.querySelector('#card-template').content.firstEleme
 /*-------------------------------- Functions -----------------------------------*/
 
 function closePopup() {
-    profileEditModal.classList.remove("modal__opened");
+    profileEditModal.classList.remove("modal_opened");
 };
 
 function getCardElement(cardData) {
@@ -68,7 +68,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
     profileTitleInput.value = profileTitle.textContent;
     profileDescriptionInput.value = profileDescription.textContent;
-    profileEditModal.classList.add("modal__opened");
+    profileEditModal.classList.add("modal_opened");
 });
 
 profileEditCloseButton.addEventListener("click", closePopup);
