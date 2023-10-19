d40431f868b66421922644c1405a40c36077fd74
quithro <143888319+quithro@users.noreply.github.com>
2023-10-19 15:55:18 -0500

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -30,18 +30,34 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const addImageModal = document.querySelector('#add-image-modal');
+
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector("#profile-description-input");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const addImageForm = addImageModal.querySelector('.modal__form');
+
 const cardListEl = document.querySelector('.cards__list');
 const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
+
+const imageAddButton = document.querySelector('#profile__add-button');
+const imageAddModal = document.querySelector('#add-image-modal');
+const imageAddCloseButton = imageAddModal.querySelector(".modal__close");
+
+
+const cardTitleInput = addImageForm.querySelector('#add-image-title-input');
+const cardUrlInput = addImageForm.querySelector('#add-image-url-input');
 
 /*-------------------------------- Functions -----------------------------------*/
 
 function closePopup() {
     profileEditModal.classList.remove("modal_opened");
+};
+
+function imageClosePopup() {
+   imageAddModal.classList.remove("modal_opened");
 };
 
 function getCardElement(cardData) {
@@ -54,6 +70,11 @@ function getCardElement(cardData) {
     return cardElement;
 };
 
+function renderCard(cardData, card) {
+    const cardElement = getCardElement(cardData);
+    cardListEl.prepend(cardElement);
+};
+
 /*--------------------------------- Event Handlers --------------------------------*/
 
 function handleProfileEditSubmit(e) {
@@ -63,7 +84,22 @@ function handleProfileEditSubmit(e) {
     closePopup();
 };
 
+function handleAddImageSubmit(e) {
+    e.preventDefault();
+    const titleValue = cardTitleInput.value;
+    const urlValue = cardUrlInput.value;
+    const cardElement = getCardElement({
+        name, link,
+    });
+    cardListEl.prepend(cardElement);
+    imageClosePopup();
+};
+
 /*--------------------------------- Event Listeners --------------------------------*/
+profileEditCloseButton.addEventListener("click", closePopup);
+imageAddCloseButton.addEventListener("click", imageClosePopup);
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+addImageForm.addEventListener('submit', handleAddImageSubmit);
 
 profileEditButton.addEventListener("click", () => {
     profileTitleInput.value = profileTitle.textContent;
@@ -71,9 +107,9 @@ profileEditButton.addEventListener("click", () => {
     profileEditModal.classList.add("modal_opened");
 });
 
-profileEditCloseButton.addEventListener("click", closePopup);
-
-profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+imageAddButton.addEventListener("click", () => {
+    imageAddModal.classList.add("modal_opened");
+});
 
 initialCards.forEach((cardData) => {
     const cardElement = getCardElement(cardData);
