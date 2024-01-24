eb1e71ad15607e678d7e33a042c44fb3f3b27675
quithro <143888319+quithro@users.noreply.github.com>
2024-01-23 18:26:52 -0600

    Add files via upload

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -9,7 +9,7 @@ export default class PopupWithImage extends Popup {
     );
   }
 
-  open(link, name) {
+  open(name, link) {
     this._popupImage.src = link;
     this._popupImage.alt = name;
     this._popupTitle.textContent = name;
