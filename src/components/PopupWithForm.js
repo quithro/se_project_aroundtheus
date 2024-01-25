59a187b8d4b4faab214fcde8b1566d44c71942cd
quithro <143888319+quithro@users.noreply.github.com>
2024-01-24 18:30:19 -0600

    Add files via upload

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,10 +5,10 @@ export default class PopupWithForm extends Popup {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
     this._popupForm = this._popupElement.querySelector(".modal__form");
-    this._inputs = this._popupElement.querySelectorAll(".modal__input");
-    this._submitButton = this._popupElement.querySelector(
-      ".modal__button"
+    this._inputs = Array.from(
+      this._popupForm.querySelectorAll(".modal__input"),
     );
+    this._submitButton = this._popupElement.querySelector(".modal__button");
     this._submitButtonText = this._submitButton.textContent;
   }
 
@@ -18,11 +18,9 @@ export default class PopupWithForm extends Popup {
   }
 
   _getInputValues() {
-    const _inputs = Array.from(
-      this._popupForm.querySelectorAll(".modal__input")
-    );
+    const _inputs = this._inputs;
     this._inputValues = {};
-      _inputs.forEach((input) => {
+    _inputs.forEach((input) => {
       this._inputValues[input.name] = input.value;
     });
     return this._inputValues;
@@ -42,7 +40,5 @@ export default class PopupWithForm extends Popup {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
     });
-    
   }
-  
 }
